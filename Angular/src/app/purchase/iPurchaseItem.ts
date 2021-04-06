53e2d7dd2e7a10df22ab5b3dfa8d19ea77a6466c
harshitvarshney1 <72073964+harshitvarshney1@users.noreply.github.com>
2021-04-06 18:23:33 +0530

    Delete Angular directory

--- Angular/src/app/purchase/iPurchaseItem.ts
+++
@@ -1,8 +0,0 @@
-import {Product} from "../product/iProduct";
-export interface iPurchaseItem {
-    Id : number,
-    Name : string,
-    PurchaseDate :string;
-    VenderName : string;
-    Items :Product[]
-}
