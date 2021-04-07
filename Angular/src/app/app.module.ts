28c8b31bbbda16d8906736ae485d07c353d38030
harshitvarshney1 <harshit.varshney@taazaa.com>
2021-04-07 18:21:53 +0530

    Angular

---
+++ Angular/src/app/app.module.ts
@@ -0,0 +1,64 @@
+import { ErrorHandler, NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
+import { SdbComponent } from './sdb/sdb.component';
+import { ProductComponent } from './product/product.component';
+import { PurchaseComponent } from './purchase/purchase.component';
+import { PruchaseorderComponent } from './pruchaseorder/pruchaseorder.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { Child1Component } from './child1/child1.component';
+import { Child2Component } from './child2/child2.component';
+import { ChangecolorDirective } from './changecolor.directive';
+import { AttributedirectiveComponent } from './attributedirective/attributedirective.component';
+import { SharedataComponent } from './sharedata/sharedata.component';
+import { HomeComponent } from './home/home.component';
+import { GenerateerrorComponent } from './generateerror/generateerror.component';
+import { MyerrorhandlerService } from './myerrorhandler.service';
+import { LoginComponent } from './login/login.component';
+import { ProductsearchPipe } from './productsearch.pipe';
+import { CustompipePipe } from './custompipe.pipe';
+import { RxjsComponent } from './rxjs/rxjs.component';
+import { BehaviourComponent } from './behaviour/behaviour.component';
+import { ReplayComponent } from './replay/replay.component';
+import { AsyncComponent } from './async/async.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    SdbComponent,
+    ProductComponent,
+    PurchaseComponent,
+    PruchaseorderComponent,
+    Child1Component,
+    Child2Component,
+    ChangecolorDirective,
+    AttributedirectiveComponent,
+    SharedataComponent,
+    HomeComponent,
+    GenerateerrorComponent,
+    LoginComponent,
+    ProductsearchPipe,
+    CustompipePipe,
+    RxjsComponent,
+    BehaviourComponent,
+    ReplayComponent,
+    AsyncComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatExpansionModule,
+    ReactiveFormsModule
+  ],
+  providers: [{ provide:ErrorHandler, useClass :MyerrorhandlerService}],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Angular/src/app/attributedirective/attributedirective.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-attributedirective',
+  templateUrl: './attributedirective.component.html',
+  styleUrls: ['./attributedirective.component.css']
+})
+export class AttributedirectiveComponent implements OnInit {
+
+  constructor() { }
+
+  c : string = 'yellow';
+  ngOnInit(): void {
+  }
+
+}
